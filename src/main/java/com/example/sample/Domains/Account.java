60e148ba9e6039995dec60ab103ff8f714ef0c76
Tatsuya Yoshida <alpacagame0129@gmail.com>
2020-05-28 11:35:33 +0900

    postRequestを受け付ける部分まで実装

---
+++ src/main/java/com/example/sample/Domains/Account.java
@@ -0,0 +1,22 @@
+package com.example.sample.Domains;
+
+public class Account {
+  private String id;
+  private String password;
+
+  public String getId() {
+    return this.id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
--- src/main/java/com/example/sample/SampleApplication.java
+++ src/main/java/com/example/sample/SampleApplication.java
@@ -1,10 +1,19 @@
 package com.example.sample;
 
+import com.example.sample.Forms.AccountForm;
+import com.example.sample.Domains.Account;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 
 @SpringBootApplication
 @Controller
@@ -17,5 +26,14 @@ public class SampleApplication {
 	@GetMapping("/")
 	public String hello(){
 		return "index";
+	}
+
+	@RequestMapping(value = "create", method = RequestMethod.POST)
+	public String accountCreate(@Validated @ModelAttribute AccountForm form, BindingResult result, Model model){
+		if(result.hasErrors()) return "error";
+		Account account = new Account();
+		BeanUtils.copyProperties(form, account);
+		System.out.println(account.getId());
+		return "redirect:/";
 	}
 }
